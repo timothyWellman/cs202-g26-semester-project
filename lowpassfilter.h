f5b80066f2778127cf14f917cd09f910eef7a101
timothyWellman <89601124+timothyWellman@users.noreply.github.com>
2021-12-02 15:55:34 -0800

    created Low Pass Filter Header

---
+++ lowpassfilter.h
@@ -0,0 +1,12 @@
+//lowpassfilter.h
+//header for the low pass filter 
+//Timothy Wellman
+//inclusion protection
+#ifndef LOW_PASS_FILTER_H
+#define LOW_PASS_FILTER_H
+//include Statements
+#include "normalization.h"
+#include "audioprocessor.h"
+class LowPassFilter: public Normalization {};
+
+#endif
